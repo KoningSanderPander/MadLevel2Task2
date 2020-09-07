886853d23f63b300c5c0b2f24b1da3e7479ff3cb
Sander <sandervandenoetelaar@hotmail.com>
2020-09-07 10:32:46 +0200

    fixed styling of the quiz_question.xml,
    Added the questions to the main view on init.

--- app/src/main/java/nl/svdoetelaar/madlevel2task2/MainActivity.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel2task2/MainActivity.kt
@@ -2,9 +2,14 @@ package nl.svdoetelaar.madlevel2task2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import nl.svdoetelaar.madlevel2task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,6 +22,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
+        for (i in Question.questions.indices) {
+            questions.add(
+                Question(
+                    Question.questions[i],
+                    Question.answers[i]
+                )
+            )
+        }
 
+        binding.rvQuestions.layoutManager =
+            LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+
+        questionAdapter.notifyDataSetChanged()
     }
+
+
 }
